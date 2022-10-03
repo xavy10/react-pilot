1e135610bb424732170f5547feca1ec3fa850cd8
javier-contreras-svitla <37781894+xavy10@users.noreply.github.com>
2022-10-02 22:04:43 -0500

    [ADDED] Project Structure

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, View, Text, SafeAreaView} from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>Hello World</Text>
+    </SafeAreaView>
+  )
+}
